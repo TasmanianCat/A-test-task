e711d529c2b3913dfc7f7d9f27dfc848d14590df
Sergei Serbulatov <ssergey.tlt@gmail.com>
2024-03-26 20:25:55 +0400

    Clear form fields function (clearForm) was added and the problem with submitting an ampty json file was fixed as well.

--- scripts/main.js
+++ scripts/main.js
@@ -12,13 +12,13 @@ openBtn.addEventListener("click", (event) => {
 
 // Close the modal and show the menu button
 closeBtn.addEventListener("click", (event) => {
+  form.style.display = "grid";
+  createJobBtn.innerHTML = "Create job";
   mainModal.style.display = "none";
   viewDealinfo.style.display = "none";
-  form.style.display = "flex";
   openBtn.style.display = "flex";
   createJobBtn.style.backgroundColor = "";
   createJobBtn.style.fontSize = "";
-  createJobBtn.innerHTML = "Create job";
 });
 
 //! Some form validation for fields of the form
@@ -50,12 +50,13 @@ function validationFormFields(form) {
 
 //! Functionality: sent POST request
 const form = document.getElementById("form");
-const formData = new FormData(form);
 
 const createJobBtn = document.getElementById("createJobButton");
 
 createJobBtn.addEventListener("click", async (event) => {
   event.preventDefault();
+
+  const formData = new FormData(form);
 
   // Using the highlightInvalidFields function (I wrote it above)
   if (!validationFormFields(form)) {
@@ -88,6 +89,9 @@ createJobBtn.addEventListener("click", async (event) => {
       const responseData = await response.json();
       console.log("Form data has been sent successfully!", responseData);
 
+      // Clear form fields after form data has been sent successfully
+      clearForm();
+
       // Hide the form after form data has been sent successfully
       form.style.display = "none";
       // Show a div block with text information and a link "ViewDeal" after form data has been sent successfully
@@ -101,3 +105,106 @@ createJobBtn.addEventListener("click", async (event) => {
 
   await submitFormData();
 });
+
+//! Functionality: clear form after data has been sent successfully
+// Clear form fields after form data has been sent successfully (This function was placed above)
+function clearForm() {
+  form.reset();
+}
+
+// Clear form fields
+// function clearForm() {
+//   const firstNameInp = document.getElementById("firstName");
+//   if (firstNameInp.value !== "" && firstNameInp.value !== null) {
+//     firstNameInp.value = "";
+//     firstNameInp.style.outlineColor = "";
+//   }
+
+//   const lastNameInp = document.getElementById("lastName");
+//   if (lastNameInp.value !== "" && lastNameInp.value !== null) {
+//     lastNameInp.value = "";
+//     lastNameInp.style.outlineColor = "";
+//   }
+//   const telInp = document.getElementById("tel");
+//   if (telInp.value !== "" && telInp.value !== null) {
+//     telInp.value = "";
+//     telInp.style.outlineColor = "";
+//   }
+
+//   const emailInp = document.getElementById("email");
+//   if (emailInp.value !== "" && emailInp.value !== null) {
+//     emailInp.value = "";
+//     emailInp.style.outlineColor = "";
+//   }
+
+//   const selectJobInp = document.getElementById("selectJob");
+//   if (selectJobInp.value !== "" && selectJobInp.value !== null) {
+//     selectJobInp.value = "0";
+//     selectJobInp.style.outlineColor = "";
+//   }
+
+//   const selectAddInfoInp = document.getElementById("selectAddInfo");
+//   if (selectAddInfoInp.value !== "" && selectAddInfoInp.value !== null) {
+//     selectAddInfoInp.value = "0";
+//     selectAddInfoInp.style.outlineColor = "";
+//   }
+
+//   const descriptionInp = document.getElementById("description");
+//   if (descriptionInp.value !== "" && descriptionInp.value !== null) {
+//     descriptionInp.value = "";
+//     descriptionInp.style.outlineColor = "";
+//   }
+
+//   const addressInp = document.getElementById("address");
+//   if (addressInp.value !== "" && addressInp.value !== null) {
+//     addressInp.value = "";
+//     addressInp.style.outlineColor = "";
+//   }
+
+//   const cityInp = document.getElementById("city");
+//   if (cityInp.value !== "" && cityInp.value !== null) {
+//     cityInp.value = "";
+//     cityInp.style.outlineColor = "";
+//   }
+//   const stateInp = document.getElementById("state");
+//   if (stateInp.value !== "" && stateInp.value !== null) {
+//     stateInp.value = "";
+//     stateInp.style.outlineColor = "";
+//   }
+
+//   const zipCodeInp = document.getElementById("zipCode");
+//   if (zipCodeInp.value !== "" && zipCodeInp.value !== null) {
+//     zipCodeInp.value = "";
+//     zipCodeInp.style.outlineColor = "";
+//   }
+
+//   const selectAreaInp = document.getElementById("selectArea");
+//   if (selectAreaInp.value !== "" && selectAreaInp.value !== null) {
+//     selectAreaInp.value = "0";
+//     selectAreaInp.style.outlineColor = "";
+//   }
+
+//   const dateInp = document.getElementById("date");
+//   if (dateInp.value !== "" && dateInp.value !== null) {
+//     dateInp.value = "";
+//     dateInp.style.outlineColor = "";
+//   }
+//   const beginTimeInp = document.getElementById("beginTime");
+//   if (beginTimeInp.value !== "" && beginTimeInp.value !== null) {
+//     beginTimeInp.value = "";
+//     beginTimeInp.style.outlineColor = "";
+//   }
+
+//   const finishTimeInp = document.getElementById("finishTime");
+//   if (finishTimeInp.value !== "" && finishTimeInp.value !== null) {
+//     finishTimeInp.value = "";
+//     finishTimeInp.style.outlineColor = "";
+//   }
+
+//   const selectTechnicianInp = document.getElementById("selectTechnician");
+//   if (selectTechnicianInp.value !== "" && selectTechnicianInp.value !== null) {
+//     selectTechnicianInp.value = "0";
+//     selectTechnicianInp.style.outlineColor = "";
+//   }
+//   return false;
+// }
